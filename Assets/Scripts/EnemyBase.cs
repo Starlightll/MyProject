160340beeb8e856609022ec424da3784fa4a46f1
khuongldhe171300 <khuongldhe171300@fpt.edu.vn>
2025-03-11 09:22:58 +0700

    Reapply "Merge branch 'khuongld'"
    This reverts commit 5f8a30de888f766eac15cb430d2c66b7688539b7.

---
+++ Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class EnemyBase : MonoBehaviour
+{
+    public float speed;
+    public int health;
+    public int damage;
+    public float moveRange = 5f; 
+
+    protected Vector3 initialPosition; 
+    protected bool movingLeft = true; 
+
+    protected Animator animator;
+
+    public virtual void Initialize(Vector3 spawnPosition)
+    {
+        transform.position = spawnPosition;
+        initialPosition = spawnPosition;
+        gameObject.SetActive(true);
+        health = 100; 
+
+        animator = GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.SetBool("IsRunning", false); 
+        }
+
+        StartCoroutine(DelayedMove());
+    }
+
+    private IEnumerator DelayedMove()
+    {
+        yield return new WaitForSeconds(1f); 
+        if (animator != null) animator.SetBool("IsRunning", true); 
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    protected void Move()
+    {
+        if (movingLeft)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.localScale = new Vector3(-1, 1, 1); 
+        }
+        else
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            transform.localScale = new Vector3(1, 1, 1); 
+        }
+        if (transform.position.x <= initialPosition.x - moveRange)
+        {
+            movingLeft = false;
+        }
+        else if (transform.position.x >= initialPosition.x + moveRange)
+        {
+            movingLeft = true;
+        }
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        StopAllCoroutines();
+        gameObject.SetActive(false);
+        EnemySpawner.Instance.ReturnEnemyToPool(this);
+    }
+}
