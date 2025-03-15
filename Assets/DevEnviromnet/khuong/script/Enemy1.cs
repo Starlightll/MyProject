51bd0ffb409e3ba1fe88a190e1366d72b447c8a1
khuongldhe171300 <khuongldhe171300@fpt.edu.vn>
2025-03-16 00:32:59 +0700

    EnemyDame

--- Assets/DevEnviromnet/khuong/script/Enemy1.cs
+++ Assets/DevEnviromnet/khuong/script/Enemy1.cs
@@ -1,7 +1,25 @@
 using UnityEngine;
 
-public class Enemy1 : Enemy
+public class Enemy1 : Enemy, EnemyDame
 {
+
+    public void Die()
+    {
+        Debug.Log("Enemy đã chết!");
+        EnemySpawner.Instance.ReturnEnemyToPool(this);
+    }
+
+    public void TakeDame(float damage)
+    {
+        currentHp -= damage;
+        currentHp = Mathf.Max(currentHp, 0);
+        UpdateHpBar();
+        if (currentHp <= 0)
+        {
+            Die();
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
