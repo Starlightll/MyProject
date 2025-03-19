15e0b20cdda17959ced8d2b44fdaf27574013b93
TranBaBinh <fanjfla1989@gmail.com>
2025-03-20 02:44:34 +0700

    enemy ban dan

---
+++ Assets/DevEnviromnet/binh/script/enemywalkercontroller.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class enemywalkercontroller : Enemy , IDamageable
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void TakeDamage(float damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Patrol()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+}
