69b307776208fd6b370493989986240e22a53b1c
TranBaBinh <fanjfla1989@gmail.com>
2025-03-21 03:58:10 +0700

    enemy walk

--- Assets/DevEnviromnet/binh/script/enemywalkercontroller.cs
+++ Assets/DevEnviromnet/binh/script/enemywalkercontroller.cs
@@ -1,37 +1,125 @@
+﻿using System;
 using UnityEngine;
 
-public class enemywalkercontroller : Enemy , IDamageable
+public class enemywalkercontroller : Enemy, IDamageable
 {
-    void Start()
-    {
+    private int direction = 1;
+    private Animator animator;
+    private float lastAttackTime;
+    private Vector2 startPosition; // Lưu vị trí bắt đầu
 
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float bulletSpeed = 10f;
+
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+        startPosition = transform.position; // Lưu vị trí bắt đầu
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-    }
-    public void TakeDamage(float damage)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    protected override void Attack()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    protected override void Die()
-    {
-        throw new System.NotImplementedException();
+        if (player != null && CheckInRange())
+        {
+            FacePlayer(); // Xoay hướng enemy về phía player
+            ShootAtPlayer();
+        }
+        else
+        {
+            Patrol();
+        }
     }
 
     protected override void Patrol()
     {
-        throw new System.NotImplementedException();
+        animator.SetBool("isMoving", true);
+        animator.SetBool("isAttacking", false);
+        transform.position += new Vector3(direction * WalkSpeed * Time.deltaTime, 0, 0);
+
+        // Kiểm tra nếu enemy di chuyển quá phạm vi được đặt trước
+        if (Mathf.Abs(transform.position.x - startPosition.x) >= PatrolRange)
+        {
+            Flip();
+        }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
+    private void ShootAtPlayer()
+    {
+        if (Time.time - lastAttackTime >= attackCooldown)
+        {
+            lastAttackTime = Time.time;
+            animator.SetTrigger("attack");
+
+            GameObject newBullet = Instantiate(bullet, firePoint.position, Quaternion.identity);
+            Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                Vector2 directionToPlayer = (player.position - firePoint.position).normalized;
+                rb.linearVelocity = directionToPlayer * bulletSpeed;
+            }
+
+            // Đảm bảo sau khi bắn, animation chuyển về trạng thái di chuyển
+            Invoke(nameof(ResetToMove), 0.5f);
+        }
+    }
+
+    protected override bool CheckInRange()
+    {
+        return Vector2.Distance(transform.position, player.position) <= AttackRange;
+    }
+
+    protected override void Flip()
+    {
+        direction *= -1;
+        transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x) * direction, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void FacePlayer()
+    {
+        if (player != null)
+        {
+            float directionToPlayer = player.position.x - transform.position.x;
+            if ((directionToPlayer > 0 && direction < 0) || (directionToPlayer < 0 && direction > 0))
+            {
+                Flip();
+            }
+        }
+    }
+
+    private void ResetToMove()
+    {
+        animator.ResetTrigger("attack");
+        animator.SetBool("isMoving", true);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        healthBar.fillAmount = currentHealth / Hp;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        animator.SetTrigger("Hurt");
+        Invoke(nameof(ResetHurt), 0.3f);
+    }
+
+    void ResetHurt()
+    {
+        animator.ResetTrigger("Hurt");
+        animator.SetBool("isHurt", false);
+    }
+
+    protected override void Die()
+    {
+        gameObject.SetActive(false);
+    }
+
+    protected override void Attack()
+    {
+        throw new NotImplementedException();
+    }
 }
