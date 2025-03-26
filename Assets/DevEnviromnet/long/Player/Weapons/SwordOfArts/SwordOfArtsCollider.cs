eaf15f90a0a15bbe36d25469939207c0a5501e38
NGUYEN HOANG LONG <145125551+Starlightll@users.noreply.github.com>
2025-03-26 20:42:52 +0700

    Improve hit calcultion for Skill1 + Improve Damage popup

--- Assets/DevEnviromnet/long/Player/Weapons/SwordOfArts/SwordOfArtsCollider.cs
+++ Assets/DevEnviromnet/long/Player/Weapons/SwordOfArts/SwordOfArtsCollider.cs
@@ -13,7 +13,7 @@ public class SwordOfArtsCollider : MonoBehaviour
         if (collider.CompareTag("Enemy"))
         {   
             _swordOfArts.hit(collider, transform.parent);
-            DamagePopup.Create(collider.transform.position, _swordOfArts.baseDamage);
+            DamagePopup.Create(collider.transform.position, _swordOfArts.baseDamage, false);
         }
     }
 
